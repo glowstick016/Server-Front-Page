0777a26141723a6b31f8e518d0146355e6b5d146
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-01-29 10:21:06 -0500

    Create passCheck.py

---
+++ Backend/Python/passCheck.py
@@ -0,0 +1,35 @@
+#Libraries
+import sys
+
+
+
+#Functions
+def passCheck(x):
+    # Function meant to check password for strength
+    # Arguments 
+    #   x: String 
+    #Return: Boolean
+    
+    #Arguments
+    passW = sys.argv[1]
+    
+    #Code
+    if(len(x) >= 10):
+        specChar = "!@#$%^&*()-+_=,<>?/"
+        chkS=chkL=chkN = False
+        for let in x:
+            if(let.isalpha()):
+                chkL = True
+            elif(let.isdigit()):
+                chkN = True
+            else:
+                for c in specChar:
+                    if(let==c):
+                        chkS = True
+                        exit
+        if(chkS==chkN==chkL==True):
+            return True
+    else:
+        return False
+
+print(passCheck(passW))
