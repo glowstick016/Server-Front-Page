57c28296b2fa37d2ca58cd24f391c453547310b8
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-02 17:15:57 -0400

    Update connect.php

--- Backend/PHP/connect.php
+++ Backend/PHP/connect.php
@@ -1,16 +1,55 @@
 <?php
+//Include Statements
+require_once "log.php";
+
+
+function addInfo($username, $email, $pass, $admin){
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+        $DB_SERVER = 'localhost:3306';
+        $DB_USERNAME = 'GlowWeb';
+        $DB_PASSWORD = 'JbLr+KSswbEpo1FF';
+        $DB_NAME = 'GlowWeb';
+
+        /* Attempt to connect to MySQL database */
+        $link = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+        // Check connection
+        if($link ->connect_error){
+                die("ERROR: Could not connect. " . mysqli_connect_error());
+                logs("ERROR: Could not connect" . mysqli_connect_error());
+        }
+
+        //User Info
+
+        $sql = "INSERT INTO Users (Usr, EMAIL, Password, Admin) VALUES ('$username', '$email', '$pass', '$admin')";
+
+        //Checking if it worked
+        if ($link->query($sql) === TRUE) {
+                echo "New user added successfully";
+        } else {
+                echo "Error: " . $sql . "<br>" . $link->error;
+                logs("Error: " . $sql . "<br>" . $link->error);
+        }
+
+        $link->close();
 }
+
+function connect(){
+                $DB_SERVER = 'localhost:3306';
+                $DB_USERNAME = 'GlowWeb';
+                $DB_PASSWORD = 'JbLr+KSswbEpo1FF';
+                $DB_NAME = 'GlowWeb';
+
+                /* Attempt to connect to MySQL database */
+                $link = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+                // Check connection
+                if($link ->connect_error){
+                        die("ERROR: Could not connect. " . mysqli_connect_error());
+                        logs("ERROR: Could not connect" . mysqli_connect_error());
+                }
+                return $link;
+}
+
 ?>
