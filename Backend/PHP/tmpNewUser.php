6b754287aa54d1912f5987bbb68fcce89e8b9b42
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-02 16:14:59 -0400

    Update tmpNewUser.php

--- Backend/PHP/tmpNewUser.php
+++ Backend/PHP/tmpNewUser.php
@@ -1,60 +1,65 @@
 <!DOCTYPE html>
 <html>
-	<head>
-    	//Basically should just add to NewUser and make an php file
-    </head>
-    
+        <head>
+        </head>
+
 <body>
-//New user
+        <h1> Application Form</h1>
+                <h2> Requirements </h2>
+                        <p>Please include name to make it easier to add (Not saved once admitted)</p>
+                        <p>Password needs the following: 10+ Char, 1 Special Char (Include one of the following !#$%^&*_+|:,.?")</p>
+
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fName">
-  Email: <input type="email" name="fEmail">
-  Password: <input type="text" name="fPass">
+  Username: <input type="text" name="fName" title="Enter your desired username (No special Chars">
+  Email: <input type="email" name="fEmail" title="Enter desired Email">
+  Password: <input type="password" name="fPass" title="Enter desired password">
   Extra Info: <input type="text" name="fExtra">
   <input type="submit">
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Configure file for MySQL
-  require_once "connect.php";
+  // Configure file for MySQL change to call a function
+        require_once "connect.php";
+        require_once "sanitize.php";
+        require_once "log.php";
 
   // collect value of input field
-  $name = htmlspecialchars($_REQUEST['fName']);
-  $email = htmlspecialchars($_REQUEST['fEmail']);
-  $pass = htmlspecialchars($_REQUEST['fPass']);
-  $extra = htmlspecialchars($_REQUEST['FExtra']);
+        $name = htmlspecialchars($_POST['fName']);
+        $email = htmlspecialchars($_POST['fEmail']);
+        $pass = htmlspecialchars($_POST['fPass']);
+        $extra = htmlspecialchars($_POST['fExtra']);
 
-	// Implement checks here
-    //Big end based on SQL length
-  	if(strlen($name) > 0 or strlen($name) <= 50){
-    } elseif($name == $name){ //Check SQL
-    } elseif($name== $name){ //Sanitiation check
-    }
-    
-    if(strlen($email) > 0 or strlen($email) <= 255){
-    } elseif($email == $email){ //Check SQL
-    } elseif($email == $email){ //Sanitiation check
-    }
-    
-    if(strlen($pass) >= 8 or strlen($pass) <= 50){
-    } elseif($pass == $pass){ //Check SQL
-    } elseif($pass == $pass){ //Sanitiation check
-    }
-    
-    if(strlen($extra) > 0 or strlen($extra) <= 50){
-    } elseif($extra == $extra){ //Check SQL
-    } elseif($extra == $extra){ //Sanitiation check
-    }
-    
-    //Implement adding to SQL
-    
-    //Add extra to txt file or smth w/ username
-    $tmpFile = fopen("Issues/../../AboutUser.txt", "a") or die("No file to open");
-    $fileSize = count($tmpFile);
-    fwrite("\n");
-    fwrite($fileSize,")");
-    fwrite($name,$email,$extra);
+        // Implement checks from sanitatize.php here
+        $usrCheck = sanitizeUser($name);
+        $emailCheck = sanitizeEmail($email);
+        $passCheck = sanitizePass($pass, $name);
+        if($usrCheck & $emailCheck & $passCheck){
+                // Add Salting for password
+
+                $filePath = "../../Issues/AboutUser.txt";
+                if (file_exists($filePath) && is_readable($filePath)) {
+
+                        $tmpFile = fopen($filePath, "a");
+
+                        if ($tmpFile){
+                                $fileSize = count(file($filePath));
+                                $text = $fileSize+1 . ")" .$name . " " . $email . " " . $extra . "\n";
+                                fwrite($tmpFile, $text);
+                                fclose($tmpFile);
+
+                                logs("Successfully added" . $name . "to the list. Waiting to be added");
+
+                                echo "Successfuly added.You will be reached out to when added or message Cam for assistance" . "\n";
+
+                        }else{echo "Failed to add reach out to Cam";}
+                }else{echo "Failed to read file reach out to Cam";}
+        }
+        else{
+                if(!$usrCheck){ echo "Failed Username check, try again";}
+                if(!$emailCheck){ echo "Failed Email check, try again";}
+                if(!$passCheck){ echo "Failed Password check, try again";}
+        }
 }
 ?>
 </body>
