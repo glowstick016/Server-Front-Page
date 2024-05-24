969bf778cae7fb4680077f8e4b5b0c32afa0dd15
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-24 13:23:08 -0400

    Create checkSes.js

---
+++ templates/js/checkSes.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	    $.ajax({
+		            url: '../Backend/PHP/getSes.php',
+		            type: 'GET',
+		            success: function(resp){
+				                if(resp.auth === 0){
+							                window.location.href = "Login.html";
+							            }
+				            }
+		        });
+});
