eeb1252f9464d871918144adc2f91d539e179807
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-24 13:24:22 -0400

    Add files via upload

--- Data/create_db.sql
+++ Data/create_db.sql
@@ -4,9 +4,11 @@ CREATE TABLE Users
 (
     Usr              varchar(50) NOT NULL,
     EMAIL            varchar(50) NOT NULL,
-    Password         varchar(50) NOT NULL,
+    Password         varchar(255) NOT NULL,
     Admin            int NOT NULL,
-    Rights           int NOT NULL,
-    Salt             VARBINARY(16),
+    Rights	     int NOT NULL,
+    Phone	     varchar(20),
+    Salt	     varchar(25),
     primary key     (Usr)
 );
+
