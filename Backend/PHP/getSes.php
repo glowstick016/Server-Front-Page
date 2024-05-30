88b4829edaf80656a10205ca7b073181b05a85a7
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-30 10:37:25 -0400

    Update getSes.php
    Added comments and cleaned up

--- Backend/PHP/getSes.php
+++ Backend/PHP/getSes.php
@@ -2,9 +2,8 @@
 
 session_start();
 
-
-
 if(isset($_SESSION['username'])){
+	//Goal: get the user session for if they're logged in and admin rights 
 
 	$tmp = array("auth" => 1, "admin" => $_SESSION['admin']);
 
