eeef0955a49682c14dd96cf2152f048931f0e15c
Cameron Kennedy <69837892+glowstick016@users.noreply.github.com>
2024-05-30 11:46:00 -0400

    Update NewUser.php
    Cleaned up and added comments

--- Backend/PHP/NewUser.php
+++ Backend/PHP/NewUser.php
@@ -6,7 +6,9 @@ error_reporting(E_ALL);
 
 
 if (isset($_POST['action']) && $_POST['action'] == 'newUser') {
-  // Configure file for MySQL change to call a function
+	//Goal: Set a new user up in the database 
+
+	// Configure file for MySQL change to call a function
 
 	require_once "connect.php";
 	require_once "sanitize.php";
@@ -27,41 +29,43 @@ if (isset($_POST['action']) && $_POST['action'] == 'newUser') {
 
 
 function processUser($name, $email, $pass, $extra){
+	//name: String - Username of user to be added
+	//email: String - Email of user to be added
+	//pass: String - Password of the user to be added
+	//extra: String - Extra info on who they are 
+
 	$resp = array("CheckUsr" => 0, "CheckEmail" => 0, "CheckPass" => 0);	
+	
 	// Implement checks from sanitatize.php here
-    	$usrCheck = sanitizeUser($name,1);
+    $usrCheck = sanitizeUser($name,1);
   	$emailCheck = sanitizeEmail($email,1);
 	$passCheck = sanitizePass($pass, $name);
 
 	//echo $usrCheck . $emailCheck . $passCheck;
 
-	if($usrCheck && $emailCheck && $passCheck){	
-		// Add Salting for password 
-		//$pass = salty($pass,$name,1);
-
-        	$filePath = "../../static/AboutUser.txt";
+	if($usrCheck && $emailCheck && $passCheck){	 
+		
+        $filePath = "../../static/AboutUser.txt";
 		if (file_exists($filePath) && is_readable($filePath)) {
+			
+			//Adding to AdboutUser
 			$tmpFile = fopen($filePath, "a");
-			//echo "File found";	
+			
         		if ($tmpFile){
                 		$fileSize = count(file($filePath));
                 		$text = $fileSize+1 . ")" . $name . " " . $email . " " . $extra . "\n"; 
 	                	fwrite($tmpFile, $text);
         	        	fclose($tmpFile);
-
-			//	echo '<div class="success-message">Successfuly added.You will be reached out to when added or message Cam for assistance</div>';
 				
-				//Add them to the database
-				addInfo($name,$email,$pass,0,0);
-				$resp = array("CheckUsr" => $usrCheck, "CheckEmail" => $emailCheck, "CheckPass" => $passCheck);
-				logs("Successfully added" . $name . "to the list. Waiting to be added\n");
-			}//else{echo '<div class="error-message"Failed to add reach out to Cam</div>';}
-		}//else{echo '<div class="error-message"Failed to add reach out to Cam</div>';}
+					//Add them to the database
+					addInfo($name,$email,$pass,0,0);
+					$resp = array("CheckUsr" => $usrCheck, "CheckEmail" => $emailCheck, "CheckPass" => $passCheck);
+					logs("Successfully added" . $name . "to the list. Waiting to be added\n");
+			}
+		}
 	}
 	else{
-	//	if(!$usrCheck){ echo '<div class="error-message"Failed Username check, try again</div>';}
-	//	if(!$emailCheck){ echo '<div class="error-message"Failed Email check, try again</div>';}
-	//	if(!$passCheck){ echo '<div class="error-message"Failed Password check, try again</div>';}
+		$resp = array("CheckUsr" => 0, "CheckEmail" => 0, "CheckPass" => 0);
 	}
 	return $resp;
 
